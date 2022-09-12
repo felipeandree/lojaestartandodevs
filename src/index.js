be48fd826f0f64373356102adfce7092cc7fda63
Atila Iglesias <atila.iglesias@hotmail.com>
2022-09-12 12:50:59 +0000

    naming & path fixes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';  
-import { Header } from './components/Header';
-import { Banner } from './components/Banner';
+import App from './App';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
-    <Banner />
+      <App />
   </React.StrictMode>
 );
 
