2525ce53e509ce7d7cb5cdac0556191731b2535c
junglereef <atila.iglesias@hotmail.com>
2022-09-19 17:51:54 -0300

    bannerComponent update installed component https://swiperjs.com/react

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,27 +1,14 @@
 import "./Home.css";
 import { Header } from "../../components/Header/Header";
-import BannerComponent from "../../components/Banner/Banner";
+import { slides, BannerComponent } from "../../components/Banner/Banner";
 import { Slider } from "../../components/Slider"
 
 const Home = () => {
-  const slides = [
-    {
-      url: "http://localhost:3000/img/banner-camisa.png",
-      title: "Slide-1",
-    },
-    {
-      url: "http://localhost:3000/img/banner-canecas.png",
-      title: "Slide-2",
-    },
-    {
-      url: "http://localhost:3000/img/banner-almofadas.png",
-      title: "Slide-3",
-    },
-  ];
+  
 
   const containerStyles = {
     width: "100%",
-    height: "720px",
+    height: "100%",
     margin: "0 auto",
   };
 
