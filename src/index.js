e5a753ffa0cc60f2282506fb00ff2e5a20c67bcb
felipe andre <felipeandre8@outlook.com>
2022-09-22 00:10:53 -0300

    navbar icons add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';  
 import App from './App';
-
 if (process.env.NODE_ENV === 'development') {
   const { worker } = require('./mocks/browser')
   worker.start()
