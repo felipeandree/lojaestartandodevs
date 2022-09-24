892da8183bbabf8e909586d11f68f06d09b0e0ac
felipe andre <felipeandre8@outlook.com>
2022-09-24 19:36:15 -0300

    Home altarado para home

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import Home from "../pages/home";
+import Home from "../pages/Home";
 import Category from "../pages/categories/category";
 
 export const Router = () => {
