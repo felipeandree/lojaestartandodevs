a0686286b4d6d4f4e115c6a180246367bda375dd
felipe andre <felipeandre8@outlook.com>
2022-09-29 18:58:16 -0300

    eslint disable lines

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -1,12 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import ProductsList from "../../components/ProductsList";
 
 export const Shop = () => {
   return (
     <>
-  
       <ProductsList/>
-
     </>
   );
 
