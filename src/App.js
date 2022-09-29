10edb935dfbf538d63c4677c4a09bb1b5130b094
Felipe André <78911918+felipeandree@users.noreply.github.com>
2022-09-29 20:34:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Routes from './routes/index'
 import GlobalStyle from './assets/styles/globalStyles';
-import { RouterProducts } from './routes/productsRoutes';
 import 'material-symbols/outlined.css';
 import { ScrollToTop } from './components/ScrollToTop/scrollToTop';
 
@@ -13,7 +12,7 @@ function App() {
       <GlobalStyle />
       < Routes />
       <ScrollToTop />
-      <RouterProducts/> 
+     
     </>
 
   );
