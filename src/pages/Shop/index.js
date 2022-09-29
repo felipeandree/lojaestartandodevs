4a970239a3318540ce36d19cd6db7515457ca287
Felipe André <78911918+felipeandree@users.noreply.github.com>
2022-09-29 19:05:18 -0300

    update index.js

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -1,9 +1,7 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import ProductsList from "../../components/ProductsList";
 
 export const Shop = () => {
-  const [ cartItems, setCartItems ] = useState([])
   return (
     <>
       <ProductsList/>
