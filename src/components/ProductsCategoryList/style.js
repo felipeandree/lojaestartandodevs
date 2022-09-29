a58c03c3fe70daa1ed6f501f524b061c8e937227
felipe andre <felipeandre8@outlook.com>
2022-09-28 23:03:26 -0300

    missing products messages

--- src/components/ProductsCategoryList/style.js
+++ src/components/ProductsCategoryList/style.js
@@ -7,6 +7,7 @@ const Main = styled.div`
   padding: 1.6rem;
   gap: 1rem; 
   align-items: center; 
+  padding-bottom: 5rem;
 `;
 
 const MainTitle = styled.h2`
@@ -18,4 +19,21 @@ const MainTitle = styled.h2`
   padding: 1.2rem 2.2rem 0.2rem;
   text-transform: capitalize;
 `;
+
+export const  MissingProducts = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 0.5rem 0.5rem 1.5rem 0.5rem;
+  gap: 0.5rem;
+`
+export const MissingMessage = styled.div`
+  text-align: left;
+  vertical-align: top;
+  font-size: 24px;
+  font-family: Open Sans;
+  line-height: 24%;
+  color: #ffffff;
+`
 export {  Main, MainTitle }
