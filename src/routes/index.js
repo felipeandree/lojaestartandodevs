0c834bb8dae20b6ac8a91eb731e9a7de303e906d
junglereef <atila.iglesias@hotmail.com>
2022-10-01 09:33:27 -0300

    fixed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Cart from "../pages/Cart";
 import Shop from "../pages/Shop";
